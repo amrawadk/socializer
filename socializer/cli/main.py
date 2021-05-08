582ddb4767847813773a0281d1ed7104e2962d7b
Amr Awad <amro.khir@gmail.com>
2021-05-08 09:58:27 +0200

    feat: add cli command to export contacts

--- socializer/cli/main.py
+++ socializer/cli/main.py
@@ -1,19 +1,21 @@
 from typing import List
 
 import typer
+from dataclass_csv import DataclassWriter
 from PyInquirer import prompt
 
 from socializer.data_augmentation import GenderClassifier
+from socializer.google_contacts import GoogleContactsAdapter
 from socializer.google_contacts.manager import GoogleContactsManager
 from socializer.google_contacts.models import GooglePerson
-from socializer.models import Gender
+from socializer.models import Contact, Gender
 
 app = typer.Typer()
 gmanager = GoogleContactsManager()
-classifier = GenderClassifier()
 
 
 def _fix_missing_genders(people: List[GooglePerson]):
+    classifier = GenderClassifier()
     for person in people:
         result = classifier.classify(name=person.given_name)
         questions = [
@@ -37,14 +39,7 @@ def _fix_missing_genders(people: List[GooglePerson]):
         )
 
 
-@app.command()
-def analyze_group(name: str, limit: int = 20):
-    typer.echo(f"Analyzing contact group: {name} ...")
-
-    people = gmanager.get_people_in_group(group_name=name, limit=limit)
-
-    typer.echo(f"Found {len(people)} people in the group")
-
+def _check_missing_gender(people: List[GooglePerson]):
     missing_gender = [c for c in people if c.gender is None]
     if missing_gender:
         questions = [
@@ -60,6 +55,35 @@ def analyze_group(name: str, limit: int = 20):
             _fix_missing_genders(people=missing_gender)
     else:
         typer.echo("All people have gender set!")
+
+
+@app.command()
+def analyze_group(name: str, limit: int = 20):
+    """Analyze Google Contacts Group and optionally add any missing data."""
+    typer.echo(f"Analyzing contact group: {name} ...")
+
+    people = gmanager.get_people_in_group(group_name=name, limit=limit)
+
+    typer.echo(f"Found {len(people)} people in the group")
+
+    _check_missing_gender(people=people)
+
+
+@app.command()
+def export_contacts(
+    group_name: str = typer.Option(...),
+    output: typer.FileTextWrite = typer.Option("contacts.csv"),
+    limit: int = 20,
+):
+    """Export Contacts in a google contact group to a csv file."""
+    gcontacts_manager = GoogleContactsAdapter()
+    contacts = gcontacts_manager.get_contacts_in_group(
+        group_name=group_name, limit=limit
+    )
+
+    writer = DataclassWriter(output, contacts, Contact)
+    writer.write()
+    typer.echo(f"contacts written to {output.name}")
 
 
 if __name__ == "__main__":
